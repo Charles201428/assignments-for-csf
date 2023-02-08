1db2e8ed014f87e66626e7fbe83cfe2253e5744f
Howard Fu <97918063+Grizzliessss@users.noreply.github.com>
2023-02-08 12:16:02 -0500

    Update uint256_tests.c

--- csf_assign01/uint256_tests.c
+++ csf_assign01/uint256_tests.c
@@ -8,10 +8,16 @@ typedef struct {
   UInt256 zero;
   UInt256 one;
   UInt256 large1;
+  UInt256 huge;
 
   const char *hex1;
   const char *hex2;
   const char *hex3;
+  const char *hex4;
+  const char *hex5;
+  const char *hex6;
+  const char *hex7;
+  const char *hex8;
 } TestObjs;
 
 // Functions to create and cleanup the test fixture object
@@ -30,11 +36,16 @@ void test_format_as_hex(TestObjs *objs);
 void test_add_1(TestObjs *objs);
 void test_add_2(TestObjs *objs);
 void test_add_3(TestObjs *objs);
+void test_add_4(TestObjs *objs);
 void test_sub_1(TestObjs *objs);
 void test_sub_2(TestObjs *objs);
 void test_sub_3(TestObjs *objs);
+void test_sub_4(TestObjs *objs);
+void test_sub_5(TestObjs *objs);
 void test_mul_1(TestObjs *objs);
 void test_mul_2(TestObjs *objs);
+void test_mul_3(TestObjs *objs);
+void test_mul_4(TestObjs *objs);
 
 int main(int argc, char **argv) {
   if (argc > 1) {
@@ -51,11 +62,16 @@ int main(int argc, char **argv) {
   TEST(test_add_1);
   TEST(test_add_2);
   TEST(test_add_3);
+  TEST(test_add_4);
   TEST(test_sub_1);
   TEST(test_sub_2);
   TEST(test_sub_3);
+  TEST(test_sub_4);
+  TEST(test_sub_5);
   TEST(test_mul_1);
   TEST(test_mul_2);
+  TEST(test_mul_3);
+  TEST(test_mul_4);
 
   TEST_FINI();
 }
@@ -80,12 +96,21 @@ TestObjs *setup(void) {
   objs->large1.data[1] = 0xCC;
   objs->large1.data[0] = 0xDD;
 
+  objs->huge.data[3] = 0xFFFFFFFFFFFFFFFF;
+  objs->huge.data[2] = 0xFFFFFFFFFFFFFFFF;
+  objs->huge.data[1] = 0xFFFFFFFFFFFFFFFF;
+  objs->huge.data[0] = 0xFFFFFFFFFFFFFFFF;
+
   // example hex strings
 
   objs->hex1 = "0";
   objs->hex2 = "cafe";
   objs->hex3 = "4a4b72ebb654226ef77ed83d884f4940e4243bc3913ceaf5781b28d25fb00b0";
-
+  objs->hex4 = "bcfeeac";
+  objs->hex5 = "10000000000000000";
+  objs->hex6 = "1000000000000000000000000000000000000000000000000000000000000000";
+  objs->hex7 = "11000000000000000000000000000000000000000000000000000000000000000";
+  objs->hex8 = "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff";
   return objs;
 }
 
@@ -145,7 +170,31 @@ void test_create_from_hex(TestObjs *objs) {
 
   val = uint256_create_from_hex(objs->hex2);
   ASSERT(check(val, 0x0UL, 0x0UL, 0x0UL, 0xcafeUL));
+
+  val = uint256_create_from_hex(objs->hex3);
+  ASSERT(check(val, 0x4a4b72ebb654226UL, 0xef77ed83d884f494UL, 0x0e4243bc3913ceafUL, 0x5781b28d25fb00b0UL));
+
+  val = uint256_create_from_hex(objs->hex4);
+  ASSERT(check(val, 0x0UL, 0x0UL, 0x0UL, 0xbcfeeacUL));
+  
+  //edge case: when the val1 is 0, while all the others are zero
+  val = uint256_create_from_hex(objs->hex5);
+  ASSERT(check(val, 0x0UL, 0x0UL, 0x1UL, 0x0UL));
+
+  val = uint256_create_from_hex(objs->hex6);
+  ASSERT(check(val, 0x1000000000000000UL, 0x0UL, 0x0UL, 0x0UL));
+
+  //edge case: when the string’s length is more than 64 characters, only use rightmost 64 hex digits
+  val = uint256_create_from_hex(objs->hex7);
+  ASSERT(check(val, 0x1000000000000000UL, 0x0UL, 0x0UL, 0x0UL));
+
+  //edge case: test the largest number
+  val = uint256_create_from_hex(objs->hex8);
+  ASSERT(check(val, 0xFFFFFFFFFFFFFFFFUL, 0xFFFFFFFFFFFFFFFFUL, 0xFFFFFFFFFFFFFFFFUL, 0xFFFFFFFFFFFFFFFFUL));
+ 
 }
+
+
 
 void test_format_as_hex(TestObjs *objs) {
   char *s;
@@ -157,6 +206,17 @@ void test_format_as_hex(TestObjs *objs) {
   s = uint256_format_as_hex(objs->one);
   ASSERT(0 == strcmp("1", s));
   free(s);
+
+  // test whether resulting hex string will not have unnecessary leading 0 digits
+  s = uint256_format_as_hex(objs->large1);
+  ASSERT(0 == strcmp("aa00000000000000bb00000000000000cc00000000000000dd", s));
+  free(s);
+
+  // edge case: test the largest number
+  s = uint256_format_as_hex(objs->huge);
+  ASSERT(0 == strcmp("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff", s));
+  free(s);
+
 }
 
 void test_add_1(TestObjs *objs) {
@@ -218,6 +278,30 @@ void test_add_3(TestObjs *objs) {
   ASSERT(0xac5151273cfcf2eUL == result.data[3]);
 }
 
+
+void test_add_4(TestObjs *objs) {
+  // "another large" addition tests
+
+  (void) objs;
+
+  UInt256 left, right, result;
+
+  // 713d3c74065a911fee8027e6ff3d6621f38d52e9636eca90324f9c1da16ea8f + 4540b2bd68d0300e607e2ded97b39c5df75b078fb51bac00948b182a75be9a4 = b67def316f2ac12e4efe55d496f1027feae85a79188a7690c6dab448172d433
+  left.data[0] = 0x0324f9c1da16ea8fUL;
+  left.data[1] = 0x1f38d52e9636eca9UL;
+  left.data[2] = 0xfee8027e6ff3d662UL;
+  left.data[3] = 0x713d3c74065a911UL;
+  right.data[0] = 0x0948b182a75be9a4UL;
+  right.data[1] = 0xdf75b078fb51bac0UL;
+  right.data[2] = 0xe607e2ded97b39c5UL;
+  right.data[3] = 0x4540b2bd68d0300UL;
+  result = uint256_add(left, right);
+  ASSERT(0x0c6dab448172d433UL == result.data[0]);
+  ASSERT(0xfeae85a79188a769UL == result.data[1]);
+  ASSERT(0xe4efe55d496f1027UL == result.data[2]);
+  ASSERT(0xb67def316f2ac12UL == result.data[3]);
+}
+
 void test_sub_1(TestObjs *objs) {
   // basic subtraction tests
 
@@ -276,6 +360,53 @@ void test_sub_3(TestObjs *objs) {
   ASSERT(0x4a4b72ebb654226UL == result.data[3]);
 }
 
+void test_sub_4(TestObjs *objs) {
+  // "another large" subtraction tests
+
+  (void) objs;
+
+  UInt256 left, right, result;
+
+  //f1e613d9abcc050fd08d24f9d4f8f051112d9101643f7c6e151e4fa9f66d079 - 76486b4f5574ca28f82b5bd6183d7016a35aa3237c1c6e2cc25ad9b1b840baa = 7b9da88a56573ae6d861c923bcbb803a6dd2eddde8230e4152c375f83e2c4cf
+  left.data[0] = 0xe151e4fa9f66d079UL;
+  left.data[1] = 0x1112d9101643f7c6UL;
+  left.data[2] = 0xfd08d24f9d4f8f05UL;
+  left.data[3] = 0xf1e613d9abcc050UL;
+  right.data[0] = 0xcc25ad9b1b840baaUL;
+  right.data[1] = 0x6a35aa3237c1c6e2UL;
+  right.data[2] = 0x8f82b5bd6183d701UL;
+  right.data[3] = 0x76486b4f5574ca2UL;
+  result = uint256_sub(left, right);
+  ASSERT(0x152c375f83e2c4cfUL == result.data[0]);
+  ASSERT(0xa6dd2eddde8230e4UL == result.data[1]);
+  ASSERT(0x6d861c923bcbb803UL == result.data[2]);
+  ASSERT(0x7b9da88a56573aeUL == result.data[3]);
+}
+
+//* test the edge case (subtracting 1 from 0 should yield a difference that is equal to the maximum value that can be represented)
+void test_sub_5(TestObjs *objs) {
+  // "large" subtraction tests
+
+  (void) objs;
+
+  UInt256 left, right, result;
+
+  //edge case: substracting 1 from 0
+  left.data[0] = 0x0UL;
+  left.data[1] = 0x0UL;
+  left.data[2] = 0x0UL;
+  left.data[3] = 0x0UL;
+  right.data[0] = 0x1UL;
+  right.data[1] = 0x0UL;
+  right.data[2] = 0x0UL;
+  right.data[3] = 0x0UL;
+  result = uint256_sub(left, right);
+  ASSERT(0xffffffffffffffffUL == result.data[0]);
+  ASSERT(0xffffffffffffffffUL == result.data[1]);
+  ASSERT(0xffffffffffffffffUL == result.data[2]);
+  ASSERT(0xffffffffffffffffUL == result.data[3]);
+}
+
 void test_mul_1(TestObjs *objs) {
   // basic multiplication tests
 
@@ -308,3 +439,37 @@ void test_mul_2(TestObjs *objs) {
   ASSERT(0x61abad710163aa9bUL == result.data[2]);
   ASSERT(0x991f2125eacd3UL == result.data[3]);
 }
+
+
+void test_mul_3(TestObjs *objs) {
+  (void) objs;
+
+  UInt256 left, right, result;
+
+  //61c1fd64715fe7b923be73100dd0edb * 943e6eedd617c75f22d8a8c7860dccb = 389bfdd59d18e4472deccde9eaf1f96c5b9a07020bbebf605b9039e22efba9
+  left.data[0] = 0x923be73100dd0edbUL;
+  left.data[1] = 0x61c1fd64715fe7bUL;
+  left.data[2] = 0x0UL;
+  left.data[3] = 0x0UL;
+  right.data[0] = 0xf22d8a8c7860dccbUL;
+  right.data[1] = 0x943e6eedd617c75UL;
+  right.data[2] = 0x0UL;
+  right.data[3] = 0x0UL;
+  result = uint256_mul(left, right);
+  ASSERT(0x605b9039e22efba9UL == result.data[0]);
+  ASSERT(0x6c5b9a07020bbebfUL == result.data[1]);
+  ASSERT(0x472deccde9eaf1f9UL == result.data[2]);
+  ASSERT(0x389bfdd59d18e4UL == result.data[3]);
+}
+
+void test_mul_4(TestObjs *objs) {
+  // test 0 * 0
+
+  UInt256 result;
+
+  result = uint256_mul(objs->zero, objs->zero);
+  ASSERT(check(result, 0UL, 0UL, 0UL, 0UL));
+
+}
+
+
