81837faf456eb4de65133ab898da367863399ba7
Charles Weng <yweng13@ugradx.cs.jhu.edu>
2023-02-19 03:08:01 -0500

    c part finished

--- csf_assign02/c_hexfuncs.c
+++ csf_assign02/c_hexfuncs.c
@@ -4,3 +4,71 @@
 #include "hexfuncs.h"
 
 // TODO: add function implementations here
+unsigned hex_read(char data_buf[]){
+    int len = 0;
+    len = read(0, data_buf, 16);
+    data_buf[len] = '\0';
+    return len;
+  }
+
+
+
+void hex_write_string(const char s[]){
+    //since we can't use strlen, we need to loop each character until the end of the string
+    int count = 0;
+    while (s[count] != '\0'){
+      write(1, s+count, 1);
+      count++;
+    }
+
+}
+
+void hex_format_offset(unsigned offset, char sbuf[]){
+    for (int i = 0; i < 8; i++){
+      //unsigned has 32 bit, and 4 bits should be stored as a hex-digit per time
+      int curdigits = 0;
+      char hex = '0';
+      //get the wanted  four bit;
+      curdigits = offset>>(4*i) & ~(~0U << 4);
+      //0-9 just get mapped to "0" - "9"
+      if (curdigits < 10){
+        hex = curdigits + 48;
+
+      }
+      else{
+        //10-15 get maps to a-f
+        hex = curdigits + 87;
+      }
+      sbuf[7-i] = hex;
+
+    }
+    sbuf[8] = '\0';
+}
+
+
+void hex_format_byte_as_hex(unsigned char byteval, char sbuf[]){
+   
+  //pretty similar to format_offset if we realize that a char is itself an int
+  for (int i = 0; i < 2; i++){
+    int curdigits = 0;
+    char hex = '0';
+    curdigits = byteval>>(4*i) & ~(~0U << 4);
+    if (curdigits < 10){
+      hex = curdigits + 48;
+    }
+    else{
+      hex = curdigits +87; 
+    }
+    sbuf[1-i] = hex;
+  }
+  sbuf[2] = '\0';
+}
+
+char hex_to_printable(unsigned char byteval){
+  //note that anything whose ascii value is smaller 32 or greater than 126 is not really printable
+  if (byteval < 32 || byteval > 126){
+    char newby = '.';
+    return newby;
+  }
+  return byteval;
+}
--- csf_assign02/c_hexmain.c
+++ csf_assign02/c_hexmain.c
@@ -1,7 +1,65 @@
 // C implementation of hexdump main function
 
 #include "hexfuncs.h"  // this is the only header file which may be included!
+//we need to print offset, one-space, hex-version of each character, 2-space, string-rep
+
+//print offset
+void printoffset(int value, char offset[]) {
+  hex_format_offset(value, offset);
+  hex_write_string(offset);
+  hex_write_string(": "); 
+  //note that this also includes the space wanted 
+}
+
+//this is prepared to print the converted printable 
+void printdata(char data[]) {
+  hex_write_string(" ");
+  hex_write_string(data);
+  hex_write_string("\n"); 
+  
+}
+
+// this function prints the space between hax pair and printables to reach allignment
+void printsuplement(int string_index) {
+  if(string_index % 16) { //this shows how many hex dig are on this line
+    int i = 0;
+    while(i < 16 - (string_index % 16)) {
+      hex_write_string("   ");
+      i++; 
+    }
+    //space generated until allignment
+  }
+}
+
+
 
 int main(void) {
-  // TODO: implement the main function
+  //this offset stores the 8 digit hex
+  char offset[9];
+  // this data stores the data read from std input
+  char data[17];
+  // manifesting how many pairs of hex appear at the current line
+  long index = 0;
+  // store the temporary pair of hex for each character in the input string
+  char hex[3];
+  //read from std input
+  int readlength = hex_read(data);
+
+  while (readlength > 0) { //update readlength at each loop by calling hex_read
+    printoffset(index, offset);
+    for(int i = 0; i < readlength; i++) {
+      //loop through every element of this string
+      hex_format_byte_as_hex(data[i], hex); 
+      hex_write_string(hex); 
+      // then we print out the hex pair
+      hex_write_string(" "); 
+      data[i] = hex_to_printable(data[i]); //conversion to printable
+      index += 1; 
+    }
+    printsupplement(index);// use space to suplemlent until allignment 
+    printdata(data);
+    //current line finished, read in data from std input to next line
+    readlength = hex_read(data); 
+  }
+  return 0;
 }
