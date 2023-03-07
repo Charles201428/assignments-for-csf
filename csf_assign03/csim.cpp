a68c162e7223247a46cb95528d021e1f5ff893fc
Charles Weng <yweng13@ugradx.cs.jhu.edu>
2023-03-06 19:59:41 -0500

    first function made

--- csf_assign03/csim.cpp
+++ csf_assign03/csim.cpp
@@ -4,6 +4,20 @@
 #include <map>
 #include <iostream>
 
+using std::string;
+using std::cin;
+using std::cerr;
+
+//this fuc sets the 
+int set_write_allocate_mode(string input){
+    string write = "write-allocate";
+    string noWrite = "no-write-allocate";
+
+    if (write == input) return 0;
+    else if (noWrite == input) return 1;
+    return -1; 
+}
+
 int main(){
     return 0;
 }
