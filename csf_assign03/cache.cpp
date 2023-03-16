1bbafd600e3e2a20526b94a5dca60fbe3c17a0c0
Charles Weng <yweng13@ugrad3.cs.jhu.edu>
2023-03-16 07:23:19 -0400

    finished

--- csf_assign03/cache.cpp
+++ csf_assign03/cache.cpp
@@ -4,19 +4,23 @@
 #define ADDR_LEN 32
 
 // Assume x >=0
-int highest_bit_set(int x) {
-    int counter = -1;
-    while (x) {
-        x >>= 1;
-	counter++;
-    }
-    return counter;
+uint32_t get_least_sig_n(uint32_t num, int n) {
+    return num & ~(~0U << n);
 }
 
-// from low to high, ignore first a bits and get the following b bits
-uint32_t get_bits(uint32_t x, int a, int b) {
-    uint32_t mask = ~(~0L << b);
-    return (mask & (x >> a)); 
+//get lowest m bits if ignoring last n bits
+uint32_t get_low_sig(uint32_t num, int n, int m) {
+    uint32_t temp = num >> n;
+    return get_least_sig_n(temp, m);
+}
+
+//fnd how many bit an uint has
+int highest_bitSet(uint num) {
+    int highest = 0;
+    while (num >>= 1) {
+        highest++;
+    }
+    return highest;
 }
 
 // Constructor
@@ -24,106 +28,117 @@ cache::cache(
         int setNum, 
         int blockNum, 
         int byteNum, 
-        bool write_allocate, // true: write-allocate | false: no-write-allocate 
-        bool write_through // true: write-through | false: write-back
-        //bool evict_type // true: lru | false: fifo
-    ): 
-        setNum(setNum), blockNum(blockNum), byteNum(byteNum), 
-        write_allocate(write_allocate), write_through(write_through), //evict_type(evict_type),
-        totalLoad(0), loadHit(0), loadMiss(0), totalStore(0), storeHit(0), storeMiss(0), cycles(0),
-        data(setNum, Set()) // need to initialize every map object inside the vector!
-    {
-        // we have already checked num_sets and num_bytes are power of 2
-        this->index_len = highest_bit_set(setNum);
-        this->offset_len = highest_bit_set(byteNum);
-        this->tag_len = ADDR_LEN - this->index_len - this->offset_len; // #tag = ADDR_LEN - #index - #offset
+        bool write_allocate, 
+        bool write_through
+    ) {
+        this->setNum = setNum;
+        this->byteNum = byteNum;
+        this->blockNum = blockNum;
+        this->write_allocate = write_allocate;
+        this->write_through = write_through;
+        totalLoad = 0;
+        totalStore = 0;
+        loadHit = 0;
+        loadMiss = 0;
+        storeHit = 0;
+        storeMiss = 0;
+        cycles = 0;
+        data = CacheType(setNum, Set());
+        index_len = highest_bitSet(setNum);
+        offset_len = highest_bitSet(byteNum);
+        tag_len = 32 - index_len - offset_len;
+        
     }
 
 
+bool cache::is_set_full(uint32_t index){
+    //find the set at given index
+    Set at_index = this->data[index];
+    //test whether the size of vector of sets is >= index
+    bool whether = at_index.size() == blockNum;
+    return whether;
+}
+
 
 bool cache::is_hit(uint32_t index, uint32_t tag) {
-	// if we can find |tag| in the keys of data[index], that means tag is in the set
-    return (data[index].find(tag) != data[index].end());
+    //find the set at given index
+    
+    Set at_index = data[index];
+    Set::iterator it = at_index.find(tag);
+    return it != at_index.end(); 
+    //return (data[index].find(tag) != data[index].end());
+
 }
 
-bool cache::is_set_full(uint32_t index) {
-	// number of blocks in a set can't exceed |num_blocks|
-  	return (data[index].size() == blockNum);
-}
+void cache::count_load_hit(uint32_t index, uint32_t tag){
+    //any access will triger lru order change
+    //find the set at given index
+    Set at_index = this->data[index];
+    //determine which counter we need to increment
+    int maxi = at_index[tag].first;
+    for (Set::iterator it = at_index.begin(); it != at_index.end(); it++) {
+        if(it->second.first < maxi) {
+            it->second.first++;
+        }
+    }
+    //set tag block to be 0-order
+    at_index[tag].first = 0;
+    data[index] = at_index;
+    //increment stuff
+    loadHit++;
+    //note that from memory trasfering each byte's consumption is assumed to be 100/4 = 25
+    cycles ++;
 
-void cache::load(uint32_t address) {
-	(this->totalLoad)++;
-	uint32_t index = get_bits(address, this->offset_len, this->index_len);
-	uint32_t tag = get_bits(address, this->offset_len+this->index_len, this->tag_len);
-	// check whether miss or not
-	if (is_hit(index, tag)) {
-		count_load_hit(index, tag);
-	} else {
-		count_load_miss(index, tag);
-	}
-}
 
-void cache::count_load_hit(uint32_t index, uint32_t tag) {
-	(this->loadHit)++; 
-	cycles++;
-	//if (evict_type == LRU) {
-		uint32_t order = (data[index])[tag].first; // get timestamp
-		for (Set::iterator it = data[index].begin();
-			it != data[index].end();
-			it++) {
-			// it->second denote the (tag, Block) pair for all blocks in the set
-			// it->second.first is the timestamp. Increment it for all elements whose timestamp
-			// was smaller than the original timestamp of the block that is hit
-			if (it->second.first < order) {
-				it->second.first++;
-			}
-		}
-		(data[index])[tag].first = 0;
-	//}
 }
 
 void cache::count_load_miss(uint32_t index, uint32_t tag) {
-	(this->loadMiss)++;
-	cycles=cycles + byteNum * 25;
+    Set at_index = data[index];
 	if (is_set_full(index)) {
 		// delete the block that should be evicted
-		uint32_t to_delete;
-		for (Set::iterator it = data[index].begin();
-			it != data[index].end();
+		uint32_t key;
+		for (Set::iterator it = at_index.begin();
+			it != at_index.end();
 			it++) {
 			if (it->second.first == (blockNum-1)) {
 				if (it->second.second == true) {
-					cycles=cycles + byteNum * 25; 
+					cycles += byteNum * 25; 
 				}
-				to_delete = it->first;
-			} 
-		}
-		data[index].erase(to_delete);
+				key = it->first;
+            }
+        }
+		at_index.erase(key);
 	}
 	// increase the counter 
-	for (Set::iterator it = data[index].begin();
-		it != data[index].end();
+	for (Set::iterator it = at_index.begin();
+		it != at_index.end();
 		it++) {
 		(it->second.first)++;
 	}
 	// add new block
-	(data[index])[tag].first = 0;
-	(data[index])[tag].second = false;
+	at_index[tag].first = 0;
+	at_index[tag].second = false;
+    data[index] = at_index;
+
+    loadMiss++;
+	cycles += byteNum * 25;
 }
 
 
+void cache::load(uint32_t address) {
+    uint32_t index = get_low_sig(address, offset_len, index_len);
+    uint32_t tag = get_low_sig(address, index_len + offset_len, tag_len);
+    if (is_hit(index, tag)) {
+        count_load_hit(index, tag);
+    }
+    else {
+        count_load_miss(index, tag);
+    }
+    totalLoad++;
 
-void cache::store(uint32_t address) {
-	(this->totalStore)++;
-	uint32_t index = get_bits(address, this->offset_len, this->index_len);
-	uint32_t tag = get_bits(address, this->offset_len+this->index_len, this->tag_len);
-	// check whether miss or not
-	if (is_hit(index, tag)) {
-		count_store_hit(index, tag);
-	} else {
-		count_store_miss(index, tag);
-	}
 }
+
+
 
 void cache::count_store_hit(uint32_t index, uint32_t tag) {
 	(this->storeHit)++;
@@ -154,33 +169,36 @@ void cache::count_store_hit(uint32_t index, uint32_t tag) {
 
 void cache::count_store_miss(uint32_t index, uint32_t tag) {
 	(this->storeMiss)++;
+    
 	if (write_allocate) {
 		// we need to change the set when it is write_allocate
 		cycles = cycles + 25 * byteNum;
-		if (is_set_full(index)) {
-			// delete the block that should be evicted
-			uint32_t to_delete;
-			for (Set::iterator it = data[index].begin();
-				it != data[index].end();
-				it++) {
-				if (it->second.first == (blockNum-1)) {
-					if (it->second.second == true) {
-						cycles=cycles + byteNum * 25; 
-					}
-					to_delete = it->first;
-				} 
-			}
-			data[index].erase(to_delete);
-		}
-		// increase the counter
-		for (Set::iterator it = data[index].begin();
-			it != data[index].end();
-			it++) {
-			(it->second.first)++;
-		}
+        Set at_index = data[index];
+	    if (is_set_full(index)) {
+		// delete the block that should be evicted
+		    uint32_t key;
+		    for (Set::iterator it = at_index.begin();
+			    it != at_index.end();
+			    it++) {
+			    if (it->second.first == (blockNum-1)) {
+				    if (it->second.second == true) {
+					    cycles += byteNum * 25; 
+				}
+				    key = it->first;
+                }
+            }
+		    at_index.erase(key);
+	}
+	    // increase the counter 
+	    for (Set::iterator it = at_index.begin();
+		    it != at_index.end();
+		    it++) {
+		    (it->second.first)++;
+	    }
 		// add new element
-		(data[index])[tag].first = 0;
-		(data[index])[tag].second = true;
+	    at_index[tag].first = 0;
+	    at_index[tag].second = false;
+        data[index] = at_index;
 		cycles++;
 		if (write_through) {
 			// write to memory
@@ -191,24 +209,27 @@ void cache::count_store_miss(uint32_t index, uint32_t tag) {
 		// when it is no-write-allocate, write to memory and cache is unchanged
 		cycles = cycles + 100;
 	}
+    (this->storeMiss)++;
 }
 
-/*
- * Format:
- * Total loads: count
- * Total stores: count
- * Load hits: count
- * Load misses: count
- * Store hits: count
- * Store misses: count
- * Total cycles: count
- */
+void cache::store(uint32_t address) { //similar to load
+    uint32_t index = get_low_sig(address, offset_len, index_len);
+    uint32_t tag = get_low_sig(address, offset_len + index_len, tag_len);
+    if (is_hit(index, tag)) {
+        count_store_hit(index, tag);
+    }
+    else {
+        count_store_miss(index, tag);
+    }
+    totalStore++;
+}
+
 void cache::output() {
-    std::cout << "Total loads: " << this->totalLoad << std::endl;
-    std::cout << "Total stores: " << this->totalStore << std::endl;
-    std::cout << "Load hits: " << this->loadHit << std::endl;
-    std::cout << "Load misses: " << this->loadMiss << std::endl;
-    std::cout << "Store hits: " << this->storeHit << std::endl;
-    std::cout << "Store misses: " << this->storeMiss << std::endl;
-    std::cout << "Total cycles: " << this->cycles << std::endl;
+    std::cout << "Total loads: " << totalLoad << std::endl;
+    std::cout << "Total stores: " << totalStore << std::endl;
+    std::cout << "Load hits: " << loadHit << std::endl;
+    std::cout << "Load misses: " << loadMiss << std::endl;
+    std::cout << "Store hits: " << storeHit << std::endl;
+    std::cout << "Store misses: " << storeMiss << std::endl;
+    std::cout << "Total cycles: " << cycles << std::endl;
 }
