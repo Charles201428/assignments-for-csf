88535043dc59c2a1f2c75125fcd1498df94e7c0c
Charles Weng <yweng13@ugrad3.cs.jhu.edu>
2023-04-10 15:26:12 -0400

    merge_sort done

--- csf_assign04/csf_assign04/parsort.c
+++ csf_assign04/csf_assign04/parsort.c
@@ -11,9 +11,13 @@
 #include <string.h>
 
 
-int cmpfunc (const void * a, const void * b) {
-   return ( *(int*)a - *(int*)b );
+
+//cmp function required for qsort
+int compare_i64 (const void * a, const void * b) {
+   return ( *(int64_t*)a - *(int64_t*)b );
 }
+
+
 
 
 // Merge the elements in the sorted ranges [begin, mid) and [mid, end),
@@ -43,24 +47,80 @@ void merge(int64_t *arr, size_t begin, size_t mid, size_t end, int64_t *temparr)
   }
 }
 
+
+
+
+
 void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
   // TODO: implement
-  size_t prob_size = end - begin + 1;
+  size_t prob_size = end - begin;
+  size_t mid = (end + begin) / 2;
+  pid_t leftkid, rightkid;
   if (prob_size <= threshold) {
-    qsort(arr + begin, prob_size, sizeof(int64_t), cmpfunc);
+    qsort(arr + begin, prob_size, sizeof(int64_t), compare_i64);
   }
-  else {
-    pid_t leftkid, rightkid;
-    leftkid = fork();
-    if (leftkid == -1) {
-      fprintf(stderr, "%s", "Error: failure to create a child process using fork.\n");
-      exit(-1);
-    }
-    else if (leftkid == 0) {
-      
-    }
+  
+  leftkid = fork(); //kid process for left kid
+  if (leftkid == -1) {
+    perror("Error: failure to create a child process using fork.\n");
+    exit(-1);
   }
+  else if (leftkid == 0) {
+    merge_sort(arr, begin, mid, threshold);
+    exit(0);
+  }
+  
+  rightkid = fork(); //right kid
+  if (rightkid == -1) {
+    perror("Error: failure to creat a child process using fork.\n");
+    exit(1);
+  }
+  else if (rightkid == 0) {
+    merge_sort(arr, begin + prob_size/2, end, threshold);
+    exit(0);
+  }
+
+
+  int left_wstatus, right_wstatus;
+  pid_t left_actual, right_actual;
+  left_actual = waitpid(leftkid, &left_wstatus, 0);
+  right_actual = waitpid(rightkid, &right_wstatus, 0);
+  if (left_actual == -1) {
+    perror("Error: waitpid left kid fails.\n");
+    exit(-2);
+  }
+  else if (!WIFEXITED(left_wstatus)) {
+    perror("left kid subprocess crashed, was interrupted, or did not exit normally\n");
+    exit(-3);
+  }
+  else if (WIFEXITED(left_wstatus) != 0) {
+    perror("left kid subprocess returned a non-zero exit code.\n");
+    exit(-4);
+  }
+  if (right_actual == -1) {
+    perror("Error: waitpid right kid fails.\n");
+    exit(2);
+  }
+  else if (!WIFEXITED(right_wstatus)) {
+    perror("right kid subprocess crashed, was interrupted, or did not exit normally\n");
+    exit(3);
+  }
+  else if (WIFEXITED(right_wstatus) != 0) {
+    perror("right kid subprocess returned a non-zero exit code.\n");
+    exit(4);
+  }
+  
+  //merging part
+  int64_t* temp = (int64_t *) malloc(prob_size * sizeof(int64_t));
+  merge(arr, begin, mid, end, temp);
+  for (int i = 0; i < prob_size; i++) {
+    arr[i] = temp[i];
+  }
+
 }
+
+
+
 
 int main(int argc, char **argv) {
   // check for correct number of command line arguments
