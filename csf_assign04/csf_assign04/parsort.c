3240bc9d330cae8433c7a7c0c39762e164ff5f54
Charles Weng <yweng13@ugrad3.cs.jhu.edu>
2023-04-10 16:08:19 -0400

    shit

--- csf_assign04/csf_assign04/parsort.c
+++ csf_assign04/csf_assign04/parsort.c
@@ -1,3 +1,10 @@
+/*
+ * C implementation of merge sort utilizing parallelism
+ * CSF Assignment 4 
+ * Bill Chen, Kit Ao
+ * jchen363@jh.edu, mao2@jh.edu
+ */
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
@@ -9,118 +16,130 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 
+/**
+ *  A funtion that compares two elements which is used for qsort
+ *  
+ *  Parameters:
+ *    left - a pointer pointing to the left element
+ *    right - a pointer pointing to the right element
+ */
+int compare_int64(const void* left, const void* right) {
+  int64_t left_num = *(const int64_t *) left;
+  int64_t right_num = *(const int64_t *) right;
 
-
-//cmp function required for qsort
-int compare_i64 (const void * a, const void * b) {
-   return ( *(int64_t*)a - *(int64_t*)b );
+  if (left_num < right_num) {
+    return -1;
+  } else if (left_num > right_num) {
+    return 1;
+  }
+  return 0;
 }
 
-
-
-
-// Merge the elements in the sorted ranges [begin, mid) and [mid, end),
-// copying the result into temparr.
-void merge(int64_t *arr, size_t begin, size_t mid, size_t end, int64_t *temparr) {
-  int64_t *endl = arr + mid;
-  int64_t *endr = arr + end;
-  int64_t *left = arr + begin, *right = arr + mid, *dst = temparr;
-
-  for (;;) {
-    int at_end_l = left >= endl;
-    int at_end_r = right >= endr;
-
-    if (at_end_l && at_end_r) break;
-
-    if (at_end_l)
-      *dst++ = *right++;
-    else if (at_end_r)
-      *dst++ = *left++;
-    else {
-      int cmp = compare_i64(left, right);
-      if (cmp <= 0)
-        *dst++ = *left++;
-      else
-        *dst++ = *right++;
+/**
+ *  Merges the sorted left half with the right half
+ *  
+ *  Parameters:
+ *    arr - the pointer pointing to the array to be sorted
+ *    begin - the index of the start of the left half
+ *    mid - the index separating left and right half
+ *    end- the index of the end of the right half (exlusive)
+ *    temparr - a pointer pointing to the temporary array that stores the results of merge
+ */
+void merge(int64_t *arr, size_t begin, size_t mid, size_t end, int64_t* temparr) {
+  int i = begin, j = mid;
+  size_t len = end - begin;
+  for (size_t k = 0; k < len; k++) {
+    if (i >= mid) {
+      temparr[k] = arr[j++];
+    } else if (j >= end) {
+      temparr[k] = arr[i++];
+    } else if (arr[i] <= arr[j]) {
+      temparr[k] = arr[i++];
+    } else if (arr[i] >= arr[j]) {
+      temparr[k] = arr[j++];
     }
   }
 }
 
-
-
-
-
-void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
-  // TODO: implement
-  size_t prob_size = end - begin;
-  size_t mid = (end + begin) / 2;
-  pid_t leftkid, rightkid;
-  if (prob_size <= threshold) {
-    qsort(arr + begin, prob_size, sizeof(int64_t), compare_i64);
-  }
-  
-  leftkid = fork(); //kid process for left kid
-  if (leftkid == -1) {
-    perror("Error: failure to create a child process using fork.\n");
-    exit(-1);
-  }
-  else if (leftkid == 0) {
-    merge_sort(arr, begin, mid, threshold);
-    exit(0);
-  }
-  
-  rightkid = fork(); //right kid
-  if (rightkid == -1) {
-    perror("Error: failure to creat a child process using fork.\n");
-    exit(1);
-  }
-  else if (rightkid == 0) {
-    merge_sort(arr, begin + prob_size/2, end, threshold);
-    exit(0);
-  }
-
-
-  int left_wstatus, right_wstatus;
-  pid_t left_actual, right_actual;
-  left_actual = waitpid(leftkid, &left_wstatus, 0);
-  right_actual = waitpid(rightkid, &right_wstatus, 0);
-  if (left_actual == -1) {
-    perror("Error: waitpid left kid fails.\n");
-    exit(-2);
-  }
-  else if (!WIFEXITED(left_wstatus)) {
-    perror("left kid subprocess crashed, was interrupted, or did not exit normally\n");
-    exit(-3);
-  }
-  else if (WIFEXITED(left_wstatus) != 0) {
-    perror("left kid subprocess returned a non-zero exit code.\n");
-    exit(-4);
-  }
-  if (right_actual == -1) {
-    perror("Error: waitpid right kid fails.\n");
-    exit(2);
-  }
-  else if (!WIFEXITED(right_wstatus)) {
-    perror("right kid subprocess crashed, was interrupted, or did not exit normally\n");
-    exit(3);
-  }
-  else if (WIFEXITED(right_wstatus) != 0) {
-    perror("right kid subprocess returned a non-zero exit code.\n");
-    exit(4);
-  }
-  
-  //merging part
-  int64_t* temp = (int64_t *) malloc(prob_size * sizeof(int64_t));
+/**
+ *  Set up for merging the sorted left half with the right half
+ *  
+ *  Parameters:
+ *    arr - the pointer pointing to the array to be sorted
+ *    begin - the index of the start of the left half
+ *    mid - the index separating left and right half
+ *    end- the index of the end of the right half (exlusive)
+ */
+void merge_together(int64_t *arr, size_t begin, size_t mid, size_t end) {
+  size_t len = end - begin;
+  int64_t* temp = (int64_t *) malloc(len * sizeof(int64_t));
   merge(arr, begin, mid, end, temp);
-  for (int i = 0; i < prob_size; i++) {
-    arr[i] = temp[i];
+  for (int i = 0; i < len; ++i) {
+    arr[begin + i] = temp[i];
   }
-
+  free(temp);
 }
 
-
-
+/**
+ *  Sort the array using the mergesort algorithm.
+ *  
+ *  Parameters:
+ *    arr - the pointer pointing to the array to be sorted
+ *    begin - the index of the start of the portion to be sorted
+ *    end- the index of the end of the portion to be sorted (exlusive)
+ *    threshold - the threshold below which the data will be sorted sequentially
+ */
+void merge_sort(int64_t *arr, size_t begin, size_t end, size_t threshold) {
+  size_t len = end - begin;
+  if (len <= threshold) {
+    qsort(arr + begin, len, sizeof(int64_t), compare_int64);
+  } else if (begin < end) {
+    size_t mid = (begin + end) / 2;
+    pid_t pid_left = fork(); // First child process to sort the left half
+    if (pid_left == -1) {
+      fprintf(stderr, "Error: first fork failed to start a new process.\n");
+      exit(1);
+    } else if (pid_left == 0) {             
+      merge_sort(arr, begin, mid, threshold);  
+      exit(0);
+    }
+    pid_t pid_right = fork(); // Second child process to sort the right half
+    if (pid_right == -1) {
+      fprintf(stderr, "Error: second fork failed to start a new process.\n");
+      exit(2);
+    } else if (pid_right == 0) {              
+      merge_sort(arr, mid, end, threshold);     
+      exit(0);
+    }
+    int wstatus_left;
+    pid_t actual_pid_left = waitpid(pid_left, &wstatus_left, 0);
+    if (actual_pid_left == -1) {
+      fprintf(stderr, "Error: waitpid_left fails.\n");
+      exit(3);
+    } else if (!WIFEXITED(wstatus_left)) {
+      fprintf(stderr, "Error: subprocess for sorting the left half crashed, was interrupted, or did not exit normally.\n");
+      exit(4);
+    } else if (WEXITSTATUS(wstatus_left) != 0) {
+      fprintf(stderr, "Error: subprocess for sorting the left half exited with error.\n");
+      exit(5);
+    }
+    int wstatus_right;
+    pid_t actual_pid_right = waitpid(pid_right, &wstatus_right, 0);
+    if (actual_pid_right == -1) {
+      fprintf(stderr, "Error: waitpid_right fails.\n");
+      exit(6);
+    } else if (!WIFEXITED(wstatus_right)) {
+      fprintf(stderr, "Error: subprocess for sorting the right half crashed, was interrupted, or did not exit normally.\n");
+      exit(7);
+    } else if (WEXITSTATUS(wstatus_right) != 0) {
+      fprintf(stderr, "Error: subprocess for sorting the right half exited with error.\n");
+      exit(8);
+    }
+    merge_together(arr, begin, mid, end);
+  }
+}
 
 int main(int argc, char **argv) {
   // check for correct number of command line arguments
@@ -128,23 +147,43 @@ int main(int argc, char **argv) {
     fprintf(stderr, "Usage: %s <filename> <sequential threshold>\n", argv[0]);
     return 1;
   }
-
   // process command line arguments
   const char *filename = argv[1];
   char *end;
   size_t threshold = (size_t) strtoul(argv[2], &end, 10);
-  if (end != argv[2] + strlen(argv[2]))
-    /* TODO: report an error (threshold value is invalid) */;
-
-  // TODO: open the file
-
-  // TODO: use fstat to determine the size of the file
-
-  // TODO: map the file into memory using mmap
-
-  // TODO: sort the data!
-
-  // TODO: unmap and close the file
-
-  // TODO: exit with a 0 exit code if sort was successful
+  if (end != argv[2] + strlen(argv[2])) {
+    fprintf(stderr, "Error: the threshold value is invalid.\n");
+    return 2;
+  }
+  int fd = open(filename, O_RDWR);
+  if (fd < 0) {
+    fprintf(stderr, "Error: the file with the integers to be sorted can't be opened.\n");
+    return 3;
+  }
+  // Use fstat to determine the size of the file
+  struct stat statbuf;
+  int rc = fstat(fd, &statbuf);
+  if (rc != 0) {
+    fprintf(stderr, "Error: fstat error occurs.\n");
+    return 4;
+  }
+  size_t file_size_in_bytes = statbuf.st_size;
+  size_t len = file_size_in_bytes / sizeof(int64_t);
+  // Map the file into memory using mmap
+  int64_t *data = mmap(NULL, file_size_in_bytes, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (data == MAP_FAILED) {
+    fprintf(stderr, "Error: mmap error occurs.\n");
+    return 5;
+  }
+  // Sort the data!
+  merge_sort(data, 0, len, threshold);
+  // unmap and close the file
+  int unmap = munmap(data, file_size_in_bytes);
+  int closed = close(fd);
+  if (closed != 0 || unmap != 0){
+    fprintf(stderr, "Error: fail to unmap and close the file.\n");
+    return 6;
+  }
+  // Exit with a 0 exit code if sort was successful
+  return 0;
 }
