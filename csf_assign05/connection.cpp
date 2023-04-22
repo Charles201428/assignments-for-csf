e72243c9f6c8a9e7f4c0a368d94fbcc7fa645b1f
Chengqian Fu <97918063+Grizzliessss@users.noreply.github.com>
2023-04-21 23:18:21 -0400

    Update connection.cpp

--- csf_assign05/connection.cpp
+++ csf_assign05/connection.cpp
@@ -14,23 +14,41 @@ Connection::Connection(int fd)
   : m_fd(fd)
   , m_last_result(SUCCESS) {
   // TODO: call rio_readinitb to initialize the rio_t object
+  rio_readinitb(&m_fdbuf, fd);
 }
 
 void Connection::connect(const std::string &hostname, int port) {
   // TODO: call open_clientfd to connect to the server
   // TODO: call rio_readinitb to initialize the rio_t object
+    m_fd = open_clientfd(hostname.c_str(), std::to_string(port).c_str());
+    if (m_fd < 0) {
+      fprintf(stderr, "%s\n", "Could not connect to server");
+      exit(1);
+    }
+    // TODO: call rio_readinitb to initialize the rio_t object
+    rio_readinitb(&m_fdbuf, m_fd);
 }
 
 Connection::~Connection() {
   // TODO: close the socket if it is open
+  if (is_open()) {
+    close();
+  }
 }
 
 bool Connection::is_open() const {
   // TODO: return true if the connection is open
+  if (m_last_result == EOF_OR_ERROR) {
+    return false;
+  }
+  return true;
 }
 
 void Connection::close() {
   // TODO: close the connection if it is open
+  if (is_open()) {
+    ::close(m_fd);
+  }
 }
 
 bool Connection::send(const Message &msg) {
