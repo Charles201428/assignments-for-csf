5b96fc089e79776bc592e0a4b259f07eceae2ae1
Chengqian Fu <97918063+Grizzliessss@users.noreply.github.com>
2023-04-22 00:25:24 -0400

    Update sender.cpp

--- csf_assign05/sender.cpp
+++ csf_assign05/sender.cpp
@@ -7,51 +7,6 @@
 #include "connection.h"
 #include "client_util.h"
 
-using std::string;
-
-int main(int argc, char **argv) {
-  if (argc != 4) {
-    std::cerr << "Usage: ./sender [server_address] [port] [username]\n";
-    return 1;
-  }
-
-  std::string server_hostname;
-  int server_port;
-  std::string username;
-
-  server_hostname = argv[1];
-  server_port = std::stoi(argv[2]);
-  username = argv[3];
-
-  // TODO: connect to server
-  Connection newcon;
-  newcon.connect(server_hostname, server_port);
-  // TODO: send slogin message
-  Message sentMsg = {TAG_SLOGIN, username};
-  newcon.send(sentMsg);
-
-  Message response;
-  newcon.receive(response);
-  if (response.tag == TAG_ERR) { 
-    fprintf(stderr, "%s", response.data.c_str());
-    exit(2);
-  } else if (response.tag != TAG_OK) {
-    fprintf(stderr, "%s\n", "Wrong Tag");
-    exit(2);
-  }
-  // TODO: loop reading commands from user, sending messages to
-  //       server as appropriate
-    while (true) {
-      std::string user_input;
-      std::getline(std::cin, user_input);
-      if (send_helper(user_input, newcon) == 1) {
-        break;
-      }
-  }
-  newcon.close();
-  return 0;
-
-}
 
 int send_helper(string& input_gotten, Connection& connectio){
   Message sentMsg;
@@ -71,15 +26,59 @@ int send_helper(string& input_gotten, Connection& connectio){
       fprintf(stderr, "%s\n", "The command is not valid");
     }
   } else { 
-    sentMsg = {TAG_SENDALL, input_gotten};
+    sentMsg = {TAG_SENDALL, user_input};
   }
 
-    if (sentMsg.tag == TAG_QUIT) {
-      return;
-    } else{
+  if (sentMsg.tag != TAG_QUIT) {
       connectio.send(sentMsg);
       connectio.receive(received);
-    }
+  }
 
   return 0;
+}
+
+
+
+int main(int argc, char **argv) {
+  if (argc != 4) {
+    std::cerr << "Usage: ./sender [server_address] [port] [username]\n";
+    return 1;
+  }
+
+  std::string server_hostname;
+  int server_port;
+  std::string username;
+
+  server_hostname = argv[1];
+  server_port = std::stoi(argv[2]);
+  username = argv[3];
+
+  // TODO: connect to server
+  Connection newcon;
+  con.connect(server_hostname, server_port);
+  // TODO: send slogin message
+  Message sentMsg = {TAG_SLOGIN, username};
+  newcon.send(sentMsg);
+
+  Message response;
+  newcon.receive(response);
+  if (response.tag == TAG_ERR) { 
+    fprintf(stderr, "%s", response.data.c_str());
+    exit(2);
+  } else if (response.tag != TAG_OK) {
+    fprintf(stderr, "%s\n", "Wrong Tag");
+    exit(2);
+  }
+  // TODO: loop reading commands from user, sending messages to
+  //       server as appropriate
+    while (true) {
+      std::string user_input;
+      std::getline(std::cin, user_input);
+      if (send_helper(user_input, con) == 1) {
+        break;
+      }
+  }
+  con.close();
+  return 0;
+
 }
