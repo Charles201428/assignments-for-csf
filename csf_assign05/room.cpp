45effe2921141931f1d7fcb6c9ca407cc6036b70
Charles Weng <yweng13@ugrad4.cs.jhu.edu>
2023-04-28 17:55:19 -0400

    damn

--- csf_assign05/room.cpp
+++ csf_assign05/room.cpp
@@ -31,36 +31,3 @@ void Room::broadcast_message(const std::string &sender_username, const std::stri
     now->mqueue.enqueue(new Message(TAG_DELIVERY, data));
   }
 }
-#include "guard.h"
-#include "message.h"
-#include "message_queue.h"
-#include "user.h"
-#include "room.h"
-
-Room::Room(const std::string &room_name)
-  : room_name(room_name) {
-  pthread_mutex_init(&lock, NULL);
-}
-
-Room::~Room() {
-  pthread_mutex_destroy(&lock);
-}
-
-void Room::add_member(User *user) {
-  Guard g(lock);
-  members.insert(user);
-}
-
-void Room::remove_member(User *user) {
-  Guard g(lock);
-  members.erase(user);
-}
-
-void Room::broadcast_message(const std::string &sender_username, const std::string &message_text) {
-  Guard g(lock);
-  std::string data = room_name + ":" + sender_username + ":" + message_text;
-  for (UserSet::iterator it = members.begin(); it != members.end(); ++it) {
-    User* now = *it;
-    now->mqueue.enqueue(new Message(TAG_DELIVERY, data));
-  }
-}
